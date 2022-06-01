427ee6de03c59c08583ab4fb123c53642193ae42
Benjamin Kincaid <ben.kincaid@rocketinsights.com>
2022-06-01 09:59:19 -0400

    update module version

--- modules/test-controller-agent/main.tf
+++ modules/test-controller-agent/main.tf
@@ -145,7 +145,7 @@ resource "aws_launch_template" "agent" {
 
 module "instance_profile_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "~> 4"
+  version = "~> 5"
 
   role_name               = "agent-role-${data.aws_region.current.name}"
   create_role             = true
